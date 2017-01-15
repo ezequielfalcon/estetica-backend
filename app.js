195b53e2ce56ed8b12b4912c41ece8e333bb2ae7
Ezequiel Falcón <falconezequiel@gmail.com>
2017-01-15 19:34:37 -0300

    pruebas de hash para passwords

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var pg = require('pg');
 var bodyParser = require('body-parser');
+var bcrypt = require('bcrypt');
+// Generate a salt
+var salt = bcrypt.genSaltSync(10);
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
@@ -14,6 +17,7 @@ app.set('port', (process.env.PORT || 5000));
 app.post('/login', function (req, res) {
     var user = req.body.usuario;
     var pass = req.body.clave;
+    var hash = bcrypt.hashSync(pass, salt);
     pg.connect(process.env.DATABASE_URL, function(err, client, done){
         client.query({
             text: "SELECT * FROM comprobar_usuario($1,$2);",
@@ -27,7 +31,8 @@ app.post('/login', function (req, res) {
             else{
                 if (result.rows[0].comprobar_usuario){
                     res.send("vamo carajo");
-                    console.log("Inicio de sesión de usuario" + user);
+                    console.log (hash);
+                    console.log("Inicio de sesión de usuario " + user);
                 }
                 else{
                     res.send("logout");
