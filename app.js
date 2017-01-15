9ed8b5128fb1a6d7f043070140b83d5cbf519e57
Ezequiel Falcón <falconezequiel@gmail.com>
2017-01-15 14:59:24 -0300

    pruebas en consulta2

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 
 app.get('/login', function (request, response) {
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-        client.query('SELECT * FROM comprobar_usuario("eze","1234")', function(err, result) {
+        client.query("SELECT * FROM comprobar_usuario('eze','1234')", function(err, result) {
             done();
             if (err)
             { console.error(err); response.send("Error " + err); }
