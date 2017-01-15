ca2369c2197f8dcb9fc6c98a872f8028836f8ac7
Ezequiel Falcón <falconezequiel@gmail.com>
2017-01-15 18:02:36 -0300

    log de resultado en PaperTrail

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.post('/login', function (req, res) {
             }
             else{
                 var logueado = result[0];
+                console.log(result[0]);
                 if (logueado == "1") res.send('errLog');
                 else res.send('ok');
             }
