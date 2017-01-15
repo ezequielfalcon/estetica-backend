eb9d8d794b8ac043b9d4694e8da67658433d261d
Ezequiel Falcón <falconezequiel@gmail.com>
2017-01-15 14:39:36 -0300

    fix de puerto

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var pg = require('pg');
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 });
@@ -18,6 +20,6 @@ app.get('/login', function (request, response) {
     });
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
 });
