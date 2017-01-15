933b0b071138c4fdcc3d3cd6ac704ea424bec3ec
Ezequiel Falcón <falconezequiel@gmail.com>
2017-01-15 17:47:09 -0300

    prueba de logueo + fix de parseo y algunas boludeces del editor

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
 var express = require('express');
 var app = express();
 var pg = require('pg');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
 
+
 app.set('port', (process.env.PORT || 5000));
 
 app.post('/login', function (req, res) {
     var user = req.body.usuario;
     var pass = req.body.clave;
-    res.send('asd: ' + user);
+    pg.connect(process.env.DATABASE_URL, function(err, client, done){
+        client.query({
+            text: "SELECT * FROM comprobar_usuario('$1','$2');",
+            values:[user, pass]
+        }, function(err, result){
+            done();
+            if (err){
+                console.log(err);
+                res.send("err");
+            }
+            else{
+                var logueado = result[0];
+                res.send('logueado: ' + logueado);
+            }
+        })
+    })
 });
 
 app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
+    console.log('Backend escuchando en puerto ', app.get('port'));
 });
