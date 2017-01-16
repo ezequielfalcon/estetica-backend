c532511058ed2891bf3e40db0b7dd22261e4cede
Ezequiel Falcón <falconezequiel@gmail.com>
2017-01-16 16:11:14 -0300

    chequeo de usuario y generación de token. también incluye mod en DB para agregar rol de usuario

--- server/seguridad.js
+++ server/seguridad.js
@@ -13,21 +13,37 @@ module.exports= {
 function login(req, res, next){
     var user = req.body.usuario;
     var pass = req.body.clave;
-    db.oneOrNone("SELECT clave FROM usuarios WHERE nombre = $1;", user)
+    db.oneOrNone("SELECT usuarios.nombre, usuarios.clave, roles.nombre rol FROM usuarios INNER JOIN roles ON usuarios.id_rol = roles.id WHERE usuarios.nombre = $1;", user)
         .then(function(data){
             if (data == null){
                 console.log("Usuario inexistente intentó inciar sesión: " + user);
-                res.send({
+                res.json({
                     resultado: false,
                     mensaje: "El usuario no existe"
                 })
             }
             else{
-                console.log(data);
-                res.send({
-                    resultado: true,
-                    mensaje: "sesión iniciada!"
-                })
+                var hashDb = data.clave;
+                if (bcrypt.compareSync(req.body.clave, hashDb)){
+                    console.log("Inicio de sesión de usuario " + user);
+                    var usuarioDb = {
+                        nombre: user,
+                        rol: data.rol
+                    };
+                    var token = jwt.sign(usuarioDb, process.env.JWT_SECRET);
+                    res.json({
+                        resultado: true,
+                        mensaje: "Sesión iniciada",
+                        token: token
+                    })
+                }
+                else{
+                    console.log("Inicio de sesión no válida por usuario " + user);
+                    res.json({
+                        resultado: false,
+                        mensaje: "Credenciales no válidas"
+                    })
+                }
             }
         })
         .catch(function (err) {
