19445620d9cbeb700e6e0611098ad1c89777fc90
Ezequiel Falcón <falconezequiel@gmail.com>
2017-01-16 15:15:57 -0300

    modularizado un poco y usando pg-promise en vez de pg para la base de datos

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var app = express();
-var pg = require('pg');
 var bodyParser = require('body-parser');
-var bcrypt = require('bcrypt');
+
 var jwt    = require('jsonwebtoken');
 
 app.use( bodyParser.json() );
@@ -12,43 +11,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', (process.env.PORT || 5000));
 
 app.post('/login', function (req, res) {
-    var user = req.body.usuario;
-    var pass = req.body.clave;
-    var hashDb = null;
-    pg.connect(process.env.DATABASE_URL, function(err, client, done){
-        client.query({
-            text: "SELECT nombre, clave FROM usuarios WHERE nombre = $1;",
-            values:[user]
-        }, function(err, result){
-            done();
-            if (err){
-                console.log(err);
-                res.json({success: false, message: err});
-            }
-            if (result.rows[0] != null){
-                hashDb = result.rows[0].clave;
-                bcrypt.compare(pass, hashDb, function(err, hashRes){
-                    if(err) {
-                        console.log(err);
-                        res.json({success: false, message: err});
-                    }
-                    if (hashRes) {
-                        console.log("Inicio de sesión por usuario " + user);
-                        var token = jwt.sign(user, process.env.JWT_SECRET);
-                        res.json({success: true, message: "sesión iniciada", token: token});
-                    }
-                    else {
-                        console.log("Inicio de sesión no válido para " + user);
-                        res.json({success: false, message: "credenciales no válidas!"})
-                    }
-                })
-            }
-            else{
-                console.log("Usuario inexistente intentó iniciar sesión: " + user);
-                res.json({success: false, message: "credenciales no válidas!"})
-            }
-        })
-    });
+
+
 });
 
 app.listen(app.get('port'), function() {
---
+++ server/seguridad.js
@@ -0,0 +1,35 @@
+/**
+ * Created by eze on 16/01/17.
+ */
+var bcrypt = require('bcrypt');
+var pgp = require("pg-promise");
+var db = pgp(process.env.DATABASE_URL);
+
+module.exports= {
+    login: login
+};
+
+function login(req, res, next){
+    var user = req.body.usuario;
+    var pass = req.body.clave;
+    db.oneOrNone("SELECT clave FROM usuarios WHERE nombre = $1;", user)
+        .then(function(data){
+            if (data == null){
+                console.log("Usuario inexistente intentó inciar sesión: " + user);
+                res.send({
+                    resultado: false,
+                    mensaje: "El usuario no existe"
+                })
+            }
+            else{
+                console.log(data);
+                res.send({
+                    resultado: true,
+                    mensaje: "sesión iniciada!"
+                })
+            }
+        })
+        .catch(function (err) {
+            return next(err);
+        });
+}
