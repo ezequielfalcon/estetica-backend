c83f8b22c2a94a7d0b10639c220b406ab8e66119
Ezequiel Falcón <falconezequiel@gmail.com>
2017-01-16 16:42:29 -0300

    verificación de token en metodo get /usuarios

--- app.js
+++ app.js
@@ -9,9 +9,12 @@ app.set('port', (process.env.PORT || 5000));
 
 
 var seguridad = require('./server/seguridad.js');
+var usuarios = require('./server/usuarios.js');
 
 
 app.post('/login', seguridad.login);
+
+app.get('/usuarios', usuarios.usuarios());
 
 app.listen(app.get('port'), function() {
     console.log('Backend escuchando en puerto ', app.get('port'));
