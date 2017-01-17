bf13198f6075c383bb4a1fda5b5cb44ba2f06b41
Ezequiel Falcón <falconezequiel@gmail.com>
2017-01-17 18:43:35 -0300

    mods para usar funciones de la base de datos, es más facil que anidar funciones asíncronas en NODE

--- server/usuarios.js
+++ server/usuarios.js
@@ -77,21 +77,24 @@ module.exports = function (db) {
                 else{
                     if (decoded.rol == "admin"){
                         if (req.params.id != decoded.nombre){
-                            db.none("DELETE FROM usuarios WHERE nombre = $1;", req.params.id)
-                                .then(function(){
-                                    console.log("Usuario " + req.params.id + " borrado!!");
-                                    res.json({resultado: true, mensaje: "usuario borrado"})
+                            db.func('usuario_borrar', req.params.id)
+                                .then(function(data){
+                                    if (data.usuario_borrar){
+                                        res.json({resultado: true, mensaje: "Usuario borrado"})
+                                    }
+                                    else{
+                                        res.json({resultado: false, mensaje:"No se encuentra el usuario"})
+                                    }
                                 })
-                                .catch(function (err){
-                                    console.log(err);
-                                    res.json({resultado: false, mensaje: err})
+                                .catch(function(err){
+                                    res.json({resultado:false, mensaje: err})
                                 })
                         }
                         else res.json({resultado: false, mensaje: "no se puede borrar a sí mismo"});
                     }
                     else{
                         console.log("Usuario " + decoded.nombre + " no autorizado");
-                        res.json({resultado: false, mensaje:"no tiene permiso para crear usuarios!"});
+                        res.json({resultado: false, mensaje:"no tiene permiso para borrar usuarios!"});
                     }
                 }
             });
@@ -106,6 +109,10 @@ module.exports = function (db) {
 
     function nuevoUsuario (req, res){
         var token = req.headers['x-access-token'];
+        if (!req.body.usuario && !req.body.clave && req.body.rol){
+            console.log("Usuario POST sin todos los datos necesarios");
+            res.json({resultado: false, mensaje: "Faltan datos en el POST"})
+        }
         if (token){
             jwt.verify(token, process.env.JWT_SECRET, function(err, decoded){
                 if (err){
@@ -115,50 +122,22 @@ module.exports = function (db) {
                 else{
                     if (decoded.rol == "admin"){
                         console.log("Usuario " + decoded.nombre + " autorizado");
-                        var rolExiste = db.oneOrNone("SELECT id FROM roles WHERE nombre = $1;", req.body.rol)
-                            .then(function(data){
-                                if (data == null) return null;
-                                return data.id;
+                        var hash = bcrypt.hashSync(req.body.clave, 10);
+                        db.func('usuario_crear', req.body.usuario, hash, req.body.rol)
+                            .then(function (data){
+                                if (data.usuario_crear == 'error-rol'){
+                                    console.log("Intento de crear usuario con rol no existente!");
+                                    res.json({resultado: false, mensaje: "El rol especificado no existe"})
+                                }
+                                if (data.usuario_crear == 'error-usuario'){
+                                    console.log("Intento de crear usuario repetido");
+                                    res.json({resultado: false, mensaje: "El nombre de usuario ya existe"})
+                                }
                             })
-                            .catch(function(err){
+                            .catch(function (err){
                                 console.log(err);
-                                return null;
-                            });
-                        var rolExisteValor = Promise.resolve(rolExiste);
-                        console.log(rolExisteValor);
-                        if (rolExiste == null){
-                            db.one("INSERT INTO roles (nombre) VALUES ($1) RETURNING id;", req.body,rol)
-                                .then(function(data){
-                                    rolExiste = data.id;
-                                })
-                                .catch(function(err){
-                                    console.log(err);
-                                    res.json({resultado: false, mensaje: err});
-                                })
-                        }
-                        var usuarioExiste = db.oneOrNone("SELECT nombre FROM usuarios WHERE nombre = $1", req.body.usuario)
-                            .then(function(data){
-                                return data == null;
+                                res.json({resultado: false, mensaje: err})
                             })
-                            .catch(function(err){
-                                console.log(err);
-                                return false;
-                            });
-                        if (!usuarioExiste){
-                            db.none("INSERT INTO usuarios (nombre, rol) VALUES ($1, $2", req.body.usuario, rolExiste)
-                                .then(function(){
-                                    console.log("Se creó el usuario " + req.body.usuario + " con el rol " + req.body.rol);
-                                    res.json({resultado: true, mensaje: "Usuario creado!"});
-                                })
-                                .catch(function(err){
-                                    console.log(err);
-                                    res.json({resultado: false, mensaje: err});
-                                })
-                        }
-                        else{
-                            console.log("Intento de crear usuario con nombre repetido");
-                            res.json({resultado: false, mensaje: "El usuario ya existe!"});
-                        }
                     }
                     else{
                         console.log("Usuario " + decoded.nombre + " no autorizado");
