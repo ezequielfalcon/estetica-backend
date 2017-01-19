ae1331759b6a2d66df9a11da25ce13b5d91d748b
Ezequiel Falcón <falconezequiel@gmail.com>
2017-01-18 21:11:30 -0300

    crear obra social

--- app.js
+++ app.js
@@ -2,25 +2,30 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var pgp = require("pg-promise")();
+var db = pgp(process.env.DATABASE_URL);
+var seguridad = require('./server/seguridad.js')(db);
+var usuarios = require('./server/usuarios.js')(db, pgp);
 
 
 app.use( bodyParser.json() );
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', (process.env.PORT || 5000));
 
-var db = pgp(process.env.DATABASE_URL);
 
-var seguridad = require('./server/seguridad.js')(db);
-var usuarios = require('./server/usuarios.js')(db, pgp);
-
-
+//login
 app.post('/api/login', seguridad.login);
 
+
+//usuarios
 app.get('/api/usuarios', usuarios.usuarios);
 app.get('/api/usuarios/:id', usuarios.usuario);
 app.post('/api/usuarios', usuarios.nuevoUsuario);
 app.delete('/api/usuarios/:id', usuarios.borrarUsuario);
 app.put('/api/usuarios/:id', usuarios.modificarUsuario);
+
+
+//obras sociales
+
 
 app.get('/api', function (req, res) {
     res.json({mensaje: "Backend del sistema!!"})
---
+++ server/obras_sociales.js
@@ -0,0 +1,57 @@
+/**
+ * Created by eze on 18/01/17.
+ */
+var jwt = require('jsonwebtoken');
+
+module.exports = function(db, pgp){
+    var module = {};
+    var qrm = pgp.queryResult;
+
+    module.crear = crear;
+
+    function crear(req, res){
+        var token = req.headers['x-access-token'];
+        if (token){
+            jwt.verify(token, process.env.JWT_SECRET, function(err, decoded){
+                if (err){
+                    console.log("Error de autenticación, token inválido!\n" + err);
+                    res.status(401).json({resultado: false, mensaje: "Error de autenticación"});
+                }
+                else{
+                    console.log("Usuario " + decoded.nombre + " autorizado");
+                    if (decoded.rol == "admin"){
+                        if (req.body.nombre){
+                            db.func("obra_social_crear", req.body.nombre, qrm.one)
+                                .then(function(data){
+                                    if (data.obra_social_crear == 'error-obra'){
+                                        res.status(400).json({resultado: false, mensaje: "ya existe una Obra Social con ese nombre"})
+                                    }
+                                    else if (data.obra_social_crear == 'ok') {
+                                        res.json({resultado: true, mensaje: "Obra Social creada", id: data.obra_social_crear})
+                                    }
+                                    else{
+                                        console.log("Error en crear_obra_social: " + data.obra_social_crear);
+                                        res.status(500).json({resultado: false, mensaje: "error no especificado:" + data.obra_social_crear})
+                                    }
+                                })
+                        }
+                        else{
+                            console.log("Obra social POST sin todos los datos necesarios");
+                            res.status(400).json({resultado: false, mensaje: "Faltan datos en el POST"})
+                        }
+                    }
+                    else{
+                        console.log("Usuario " + decoded.nombre + " no autorizado");
+                        res.status(403).json({resultado: false, mensaje:"no tiene permiso para crear Obras Sociales!"});
+                    }
+                }
+            });
+        }
+        else{
+            res.status(401).json({
+                resultado: false,
+                mensaje: 'No token provided.'
+            });
+        }
+    }
+};
