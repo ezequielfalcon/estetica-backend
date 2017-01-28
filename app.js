de24db1f54e9f1ef6f8584f239d625bf690aea74
Ezequiel Falcón <falconezequiel@gmail.com>
2017-01-28 00:22:52 -0300

    otro fix en headers (access control)

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var pacientes = require('./server/pacientes')(db, pgp);
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
+    res.header("Access-Control-Allow-Methods", "POST, PUT, DELETE, GET, OPTIONS");
     next();
 });
 
