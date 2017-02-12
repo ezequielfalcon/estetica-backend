cc999b19289a2cd288c4c03473a1161b578868c7
Ezequiel Falcón <falconezequiel@gmail.com>
2017-02-12 14:25:57 -0300

    fix menor de JSON

--- server/turnos.js
+++ server/turnos.js
@@ -21,7 +21,7 @@ module.exports = function(db, pgp){
                     console.log("Usuario " + decoded.nombre + " autorizado");
                     db.func("turnos_ver_configuracion", [], qrm.one)
                         .then(function(data){
-                            res.json(data);
+                            res.json({resultado: true, datos: data});
                         })
                         .catch(function(error){
                             res.status(500).json({resultado: false, mensaje: "Error interno al ver configuraciones: " + error});
